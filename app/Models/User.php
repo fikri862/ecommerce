417e18c3c84c4b1d3f81b12ff71791317f708f93
fikri862 <fikrihaykalnasution@gmail.com>
2023-12-07 09:10:41 +0700

    create models

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'is_admin',
     ];
 
     /**
@@ -42,4 +43,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function orders () {
+        return $this->hasMany(Order::class);
+    }
+
+    public function carts () {
+        return $this->hasMany(Cart::class);
+    }
 }
