50fef20efdee6da52b7ec73e2d88597b4d0360cb
fikri862 <fikrihaykalnasution@gmail.com>
2023-12-25 10:50:41 +0700

    add show all order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -42,4 +42,9 @@ class OrderController extends Controller
 
         return Redirect::back();
     }
+
+    public function index_order () {
+        $orders = Order::all();
+        return view('index_order', compact('orders'));
+    }
 }
